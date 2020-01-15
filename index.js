0a41b8646ccb33d36d4c008c4e8cd88f054360b9
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-15 16:58:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let main = document.querySelector("main");
+document.body.removeChild(main);
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "David is the champion";
