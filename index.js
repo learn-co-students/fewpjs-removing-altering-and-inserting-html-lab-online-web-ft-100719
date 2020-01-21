152b84465a50e878fd796244f77c7f41df8250c4
Emily <emily.a.harber@gmail.com>
2020-01-20 21:07:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let mainNode = document.getElementById('main'); 
+mainNode.remove(); 
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Emily is the champion.';
+
+
