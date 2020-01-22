fe98a13e039bf185545077cc0152ab242a82fc8a
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-21 20:49:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let m = document.querySelector('main#main');
+m.remove();
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = 'YOUR-NAME is the champion'
